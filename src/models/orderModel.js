812de31f5eb5b7806d542a426a41108a5701bc3e
chiragdayal232 <96950730+chiragdayal232@users.noreply.github.com>
2024-12-07 00:08:34 +0530

    initial commit

---
+++ src/models/orderModel.js
@@ -0,0 +1,83 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    shippingInfo: {
+      address: {
+        type: String,
+        required: true,
+      },
+      city: {
+        type: String,
+        required: true,
+      },
+      state: {
+        type: String,
+        required: true,
+      },
+      country: {
+        type: String,
+        required: true,
+      },
+      pincode: {
+        type: Number,
+        required: true,
+      },
+    },
+
+    // here we are passing the user id, but this user id is not in the format of mongodb id becaue this id is provided by us or provided by firebase hence we are uysing its type as string and not mongoose.types...
+    user: {
+      type: String,
+      ref: "User",
+      required: true,
+    },
+
+    subtotal: {
+      type: Number,
+      required: true,
+    },
+    tax: {
+      type: Number,
+      required: true,
+    },
+    shippingCharges: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    discount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    total: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["Processing", "Shipped", "Delivered"],
+      default: "Processing",
+    },
+
+    orderItems: [
+      {
+        name: String,
+        description: String,
+        photo: String,
+        price: Number,
+        quantity: Number,
+        stock: Number,
+        productId: {
+          type: mongoose.Types.ObjectId,
+          ref: "Product",
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Order = mongoose.model("Order", schema);
